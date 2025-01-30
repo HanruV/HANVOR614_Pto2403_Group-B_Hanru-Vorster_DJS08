bd1775b4223a14cc339e806c329de6b50135897d
Hanru Vorster <hanru.vorster@gmail.com>
2025-01-30 13:06:26 +0200

    added a HostLayout component that will be the nav for the Links to income and reviews

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -2,10 +2,5 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
-  return (
-    <>
-      <h1>Dashboard goes here</h1>
-      <Outlet />
-    </>
-  );
+  return <h1>Dashboard goes here</h1>;
 }
