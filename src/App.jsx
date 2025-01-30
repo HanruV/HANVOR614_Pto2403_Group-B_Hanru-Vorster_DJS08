7146843e74603ddbacfe92598d387dc8b9f0065d
Hanru Vorster <hanru.vorster@gmail.com>
2025-01-30 15:58:37 +0200

    added 3 new host pages for the HostVanDetail namely HostVanInfo, HostVanPhotos, HostVanPricing.
    Also nested them in some new Routes

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import Reviews from "../pages/Host/Reviews";
 import HostLayout from "../components/HostLayout";
 import HostVans from "../pages/Host/HostVans";
 import HostVanDetail from "../pages/Host/HostVanDetail.jsx";
+import HostVanInfo from "../pages/Host/HostVanInfo.jsx";
+import HostVanPhotos from "../pages/Host/HostVanPhotos.jsx";
+import HostVanPricing from "../pages/Host/HostVanPricing.jsx";
 
 function App() {
   return (
@@ -30,7 +33,11 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
