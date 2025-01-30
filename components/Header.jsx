c0fa8365770b2c4389c3318126cc5e67db2589e0
Hanru Vorster <hanru.vorster@gmail.com>
2025-01-30 14:31:20 +0200

    added Navlinks to the header children, and addes css to style them

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
-      <Link className="site-logo" to="/">
+      <NavLink className="site-logo" to="/">
         #VanLife
-      </Link>
+      </NavLink>
       <nav>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
-        <Link to="/host">Host</Link>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/vans"
+        >
+          Vans
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/host"
+        >
+          Host
+        </NavLink>
       </nav>
     </header>
   );
