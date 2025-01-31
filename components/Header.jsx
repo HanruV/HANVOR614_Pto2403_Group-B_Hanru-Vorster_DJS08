1497ad5bfc934dc7e5187ba40aa042617c667f2a
Hanru Vorster <hanru.vorster@gmail.com>
2025-01-31 14:15:18 +0200

    mended history so once logged in and at dash it does not take us back to login when we press back but rather the home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,10 @@ import { Link, NavLink } from "react-router-dom";
 import imageUrl from "../assets/images/avatar-icon.png";
 
 export default function Header() {
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <header>
       <NavLink className="site-logo" to="/">
@@ -30,6 +34,7 @@ export default function Header() {
         <Link to="login" className="login-link">
           <img src={imageUrl} className="login-icon" />
         </Link>
+        <button onClick={fakeLogOut}>X</button>
       </nav>
     </header>
   );
